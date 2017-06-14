703854a321f9caaebb459956271ddcf40b0f4188
David Siegel <djsiegel@gmail.com>
2017-06-13 20:46:06 -0700

    Initial commit

--- gymmy/ViewController.swift
+++ gymmy/ViewController.swift
@@ -1,25 +1,54 @@
-//
-//  ViewController.swift
-//  gymmy
-//
-//  Created by David SIegel on 6/13/17.
-//  Copyright © 2017 David Siegel. All rights reserved.
-//
-
 import UIKit
+import SwiftSoup
 
-class ViewController: UIViewController {
+struct GymClass {
+    let name: String
+    let description: String
+}
 
+class ViewController: UITableViewController {
+
+    var classes = [GymClass]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        classes = (try? getClasses()) ?? []
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func getClasses() throws -> [GymClass] {
+        let url = URL(string: "https://fitnesssf.com/events/mid-market")!
+        let html = try String(contentsOf: url)
+        let doc = try SwiftSoup.parse(html)
+        var classes = [GymClass]()
+        
+        for studio in try doc.select(".schedule") {
+            for event in try studio.select(".event-description") {
+                classes.append(GymClass(
+                    name: try event.select("h3").text(),
+                    description: try event.select(".event-description-content").text()
+                ))
+            }
+        }
+        
+        return classes
     }
-
-
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return classes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "event", for: indexPath)
+        
+        let event = classes[indexPath.row]
+        cell.textLabel?.text = event.name
+        cell.detailTextLabel?.text = event.description
+        
+        return cell
+    }
 }
 
