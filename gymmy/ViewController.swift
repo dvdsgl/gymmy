505573faaa1f683f5cf016cf82b6b8d4b7616895
David Siegel <djsiegel@gmail.com>
2017-06-15 20:15:50 -0700

    Display classes by day of week

--- gymmy/ViewController.swift
+++ gymmy/ViewController.swift
@@ -2,16 +2,25 @@ import UIKit
 import SwiftSoup
 
 struct GymClass {
-    let name, description, time, trainer, studio: String
+    let name, description, day, time, trainer, studio: String
 }
 
 class ViewController: UITableViewController {
 
     var classes = [GymClass]()
     
+    var classesByDay = [String: [GymClass]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         classes = (try? getClasses()) ?? []
+        
+        classesByDay.removeAll()
+        for c in classes {
+            var cs = classesByDay[c.day] ?? []
+            cs.append(c)
+            classesByDay[c.day] = cs
+        }
     }
     
     func getClasses() throws -> [GymClass] {
@@ -28,14 +37,17 @@ class ViewController: UITableViewController {
         
         for (id, name: studioName) in studios {
             let studio = try doc.select("#\(id).schedule")
-            for event in try studio.select(".single-event") {
-                classes.append(GymClass(
-                    name: try event.select("h4").text(),
-                    description: try event.select(".event-description-content").text(),
-                    time: try event.select(".event-time").text(),
-                    trainer: try event.select(".event-trainer").text(),
-                    studio: studioName
-                ))
+            for day in try doc.select(".schedule-day-name") {
+                for event in try day.siblingElements().select(".single-event") {
+                    classes.append(GymClass(
+                        name: try event.select("h4").text(),
+                        description: try event.select(".event-description-content").text(),
+                        day: try day.text(),
+                        time: try event.select(".event-time").text(),
+                        trainer: try event.select(".event-trainer").text(),
+                        studio: studioName
+                    ))
+                }
             }
         }
         
@@ -43,17 +55,29 @@ class ViewController: UITableViewController {
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return classesByDay.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let days = [String](classesByDay.keys)
+        let day = days[section]
+        return day
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let days = [String](classesByDay.keys)
+        let day = days[section]
+        let classes = classesByDay[day] ?? []
         return classes.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "event", for: indexPath)
         
-        let event = classes[indexPath.row]
+        let days = [String](classesByDay.keys)
+        let day = days[indexPath.section]
+        
+        let event = classesByDay[day]![indexPath.row]
         cell.textLabel?.text = event.name
         cell.detailTextLabel?.text = event.description
         
