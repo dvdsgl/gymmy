a86f164269072b6fbf8b73724575ea8d1890744e
David Siegel <djsiegel@gmail.com>
2017-07-10 17:17:46 -0700

    Move persistence to own file

--- gymmy/ViewController.swift
+++ gymmy/ViewController.swift
@@ -12,50 +12,6 @@ public extension Sequence {
     }
 }
 
-class Persistence {
-    static let defaults = UserDefaults.standard
-    
-    static var studioFilter: String? {
-        get {
-            return defaults.string(forKey: "studioFilter")
-        }
-        set(newValue) {
-            defaults.set(newValue, forKey: "studioFilter")
-        }
-    }
-    
-    static func cachedHTML(for url: String) -> (String, Date)? {
-        if let html = defaults.string(forKey: "cached \(url)") {
-            if let date = defaults.value(forKey: "cached \(url) date") as? Date {
-                return (html, date)
-            }
-        }
-        return nil
-    }
-    
-    static func cacheHTML(for url: String, html: String) {
-        defaults.set(html, forKey: "cached \(url)")
-        defaults.set(Date(), forKey: "cached \(url) date")
-    }
-    
-    static func cachedOrDownload(url: String, expireAfter: TimeInterval = 60 * 60 * 24) -> String? {
-        if let (html, date) = cachedHTML(for: url) {
-            if Date() < date.addingTimeInterval(expireAfter) {
-                return html
-            }
-        }
-        
-        guard let theUrl = URL(string: url) else { return nil }
-        
-        if let html = try? String(contentsOf: theUrl) {
-            cacheHTML(for: url, html: html)
-            return html
-        }
-        
-        return nil
-    }
-}
-
 class ViewController: UITableViewController {
     var classes: [GymClass] = [] {
         didSet {
