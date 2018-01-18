bce0a80470e9deaa2454bccede41625c7e832cab
David Siegel <djsiegel@gmail.com>
2018-01-18 11:50:20 -0800

    Try again to enable distribution

--- gymmy/AppDelegate.swift
+++ gymmy/AppDelegate.swift
@@ -9,15 +9,29 @@ import AppCenterDistribute
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
-        MSAppCenter.start("e6ffcad6-61cf-4a1f-970e-7ed173493396", withServices:[
+    
+#if DEBUG
+    let debug = true;
+#else
+    let debug = false;
+#endif
+    
+    func startAppCenter() {
+        var services = [
             MSAnalytics.self,
             MSCrashes.self,
-            MSDistribute.self
-        ])
+        ]
         
+        if debug {
+            services.append(MSDistribute.self)
+        }
+        
+        MSAppCenter.start("e6ffcad6-61cf-4a1f-970e-7ed173493396", withServices:services)
+    }
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        startAppCenter()
         application.applicationSupportsShakeToEdit = true
         
         return true
